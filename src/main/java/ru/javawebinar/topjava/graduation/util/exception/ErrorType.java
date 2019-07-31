56c0ae969fb2491291c10e6c1b620c0b9957cd4e
Dimio <dimio@dimio.org>
2019-07-31 18:23:42 +0300

    added: common utilities and exceptions

---
+++ src/main/java/ru/javawebinar/topjava/graduation/util/exception/ErrorType.java
@@ -0,0 +1,20 @@
+package ru.javawebinar.topjava.graduation.util.exception;
+
+public enum ErrorType {
+    //TODO
+    APP_ERROR("error.appError"),
+    DATA_NOT_FOUND("error.dataNotFound"),
+    DATA_ERROR("error.dataError"),
+    VALIDATION_ERROR("error.validationError"),
+    WRONG_REQUEST("error.wrongRequest");
+
+    private final String errorCode;
+
+    ErrorType(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+}
---
+++ src/main/java/ru/javawebinar/topjava/graduation/util/exception/IllegalRequestDataException.java
@@ -0,0 +1,7 @@
+package ru.javawebinar.topjava.graduation.util.exception;
+
+public class IllegalRequestDataException extends RuntimeException {
+    public IllegalRequestDataException(String msg) {
+        super(msg);
+    }
+}

faa36c91cb20bfea7bdb392f65911b148e6931fe
Dimio <dimio@dimio.org>
2020-02-05 23:41:27 +0300

    Added: meal repository

---
+++ src/main/java/ru/javawebinar/topjava/graduation/repository/MealRepository.java
@@ -0,0 +1,31 @@
+package ru.javawebinar.topjava.graduation.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import ru.javawebinar.topjava.graduation.model.Meal;
+
+import java.util.Optional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface MealRepository extends JpaRepository<Meal, Integer> {
+
+    @Override
+    @Transactional
+    Meal save(Meal meal);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Meal m WHERE m.id=:id AND m.menu.id=:menuId")
+    int delete(@Param("id") int id, @Param("menuId") int menuId);
+
+    @Override
+    Optional<Meal> findById(Integer id);
+
+    Optional<Meal> findByName(String name);
+
+}

80b1e469bdf12d5a166b6f7ea1b981096615ecd9
Dimio <dimio@dimio.org>
2020-03-15 20:56:19 +0300

    refactor(Controller): accord User controllers for new API
    UserVoteController:
    - added various response codes for voting
    - added possibility to get votes for specified restaurant
    UserRestaurantController:
    - added possibility to get all restaurants (with menu items) which have actual menus for today
    - added possibility to get menus and menu items for specified restaurant

--- src/main/java/ru/javawebinar/topjava/graduation/web/restaurant/UserRestaurantController.java
+++ src/main/java/ru/javawebinar/topjava/graduation/web/restaurant/UserRestaurantController.java
@@ -2,13 +2,14 @@ package ru.javawebinar.topjava.graduation.web.restaurant;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import ru.javawebinar.topjava.graduation.model.Meal;
 import ru.javawebinar.topjava.graduation.model.Menu;
 import ru.javawebinar.topjava.graduation.model.Restaurant;
+import ru.javawebinar.topjava.graduation.service.MealService;
+import ru.javawebinar.topjava.graduation.service.MenuService;
 import ru.javawebinar.topjava.graduation.service.RestaurantService;
-import ru.javawebinar.topjava.graduation.util.exception.NotFoundException;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -19,34 +20,104 @@ import java.util.List;
     produces = MediaType.APPLICATION_JSON_VALUE)
 public class UserRestaurantController {
 
-    public static final String REST_URL = "/rest/profile/restaurants";
+    public static final String REST_URL = "/rest/restaurants";
+    public static final String REST_URL_MENUS = "/{restaurantId}/menus";
+    public static final String REST_URL_MEALS =  REST_URL_MENUS + "/{menuId}/meals";
 
     private final Logger log = LoggerFactory.getLogger(getClass());
 
     private final RestaurantService restaurantService;
+    private final MenuService menuService;
+    private final MealService mealService;
 
-    public UserRestaurantController(RestaurantService restaurantService) {
+    // TODO: 13.03.2020 add caching
+
+    public UserRestaurantController(RestaurantService restaurantService, MenuService menuService, MealService mealService) {
         this.restaurantService = restaurantService;
+        this.menuService = menuService;
+        this.mealService = mealService;
     }
 
+    /**
+     * Get restaurants with actual menus (with meals) for today
+     * @return List of restaurants, who contains menus actual for current date, with menus and meals
+     * {@link Restaurant}
+     */
     @GetMapping
     public List<Restaurant> getAllActual() {
-        log.info("get all actual restaurants");
-        return restaurantService.getAllWithMenuForDate(LocalDate.now());
+        log.info("get all restaurants with actual menus for today");
+        return restaurantService.getAllWithMenusAndMealsForDate(LocalDate.now());
     }
 
-    @GetMapping(value = "/{restaurantId}")
-    public Restaurant getRestaurantById(@PathVariable int restaurantId) {
+    /**
+     * Get restaurant information by specified ID
+     * @param restaurantId - ID (int) of restaurant
+     * @return Restaurant entity with menus and meals {@link Restaurant}
+     */
+    @GetMapping("/{restaurantId}")
+    public Restaurant getById(@PathVariable int restaurantId) {
         log.info("get restaurant {}", restaurantId);
         return restaurantService.get(restaurantId);
     }
 
-    @GetMapping(value = "/{restaurantId}/menu")
-    public List<Menu> getMenuForRestaurantForDate(@PathVariable int restaurantId,
-                                                  @RequestParam(required = false)
-                                                  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) throws NotFoundException {
-        log.info("get menu for restaurant {} today", restaurantId);
-        date = (date == null) ? LocalDate.now() : date;
-        return restaurantService.getMenusForRestaurantBetweenDate(restaurantId, date, date);
+    /**
+     * Get restaurant information by specified name
+     * @param name - name (String) of restaurant
+     * @return Restaurant entity with menus and meals {@link Restaurant}
+     */
+    @GetMapping("/byName")
+    public Restaurant getByName(@RequestParam String name) {
+        log.info("get restaurant by name {}", name);
+        return restaurantService.getByName(name);
+    }
+
+    /**
+     * Get all menus (without meals) of a restaurant as array
+     * @param restaurantId - ID (int) of restaurant
+     * @return List of menus for a restaurant with specified restaurantId
+     * {@link Menu}
+     */
+    @GetMapping(REST_URL_MENUS)
+    public List<Menu> getMenus(@PathVariable int restaurantId) {
+        log.info("get all menus for a restaurant {}", restaurantId);
+        return menuService.getAllForRestaurant(restaurantId);
+    }
+
+    /**
+     * Get menu (without meals) for restaurant
+     * @param restaurantId - ID (int) of restaurant
+     * @param menuId - ID (int) of menu
+     * @return Menu entity with specified menuId for a restaurant with specified restaurantId
+     * {@link Menu}
+     */
+    @GetMapping(REST_URL_MENUS  + "/{menuId}")
+    public Menu getMenu(@PathVariable int restaurantId, @PathVariable int menuId) {
+        log.info("get menu {} for a restaurant {}", menuId, restaurantId);
+        return menuService.getForRestaurant(restaurantId, menuId);
+    }
+
+    /**
+     * Get all menu items only (i.e. meals) for a menu as array
+     * @param menuId - ID (int) of restaurant
+     * @return List of menu items for a menu with specified menuId
+     * {@link Meal}
+     */
+    @GetMapping(REST_URL_MEALS)
+    public List<Meal> getMeals(@PathVariable int menuId) {
+        log.info("get all meals for a menu {}", menuId);
+        return mealService.getAllForMenu(menuId);
+    }
+
+    /**
+     * Get menu item (i.e. meal) for menu
+     * @param menuId - ID (int) of menu
+     * @param mealId - ID (int) of meal
+     * @return Meal entity with specified mealId for a menu with specified menuId
+     * {@link Meal}
+     */
+    @GetMapping(REST_URL_MEALS  + "/{mealId}")
+    public Meal getMeal(@PathVariable int menuId, @PathVariable int mealId) {
+        log.info("get meal {} for a menu {}", mealId, menuId);
+        return mealService.getForMenu(menuId, mealId);
     }
 }
--- src/main/java/ru/javawebinar/topjava/graduation/web/vote/UserVoteController.java
+++ src/main/java/ru/javawebinar/topjava/graduation/web/vote/UserVoteController.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -15,11 +14,15 @@ import ru.javawebinar.topjava.graduation.service.VoteService;
 
 import java.net.URI;
 import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
+
+import static ru.javawebinar.topjava.graduation.web.vote.UserVoteController.REST_URL;
 
 @RestController
-@RequestMapping(value = UserVoteController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(REST_URL)
 public class UserVoteController {
-    static final String REST_URL = "/rest/profile/votes";
+    static final String REST_URL = "/rest/votes";
 
     private final Logger log = LoggerFactory.getLogger(getClass());
 
@@ -29,37 +32,83 @@ public class UserVoteController {
         this.service = service;
     }
 
-    @RequestMapping(value = "/{restaurantId}", method = {RequestMethod.PUT, RequestMethod.POST})
-    public ResponseEntity<Vote> vote(@PathVariable int restaurantId, @AuthenticationPrincipal AuthorizedUser authUser) {
+    /**
+     * Vote for a restaurant or change own vote to another restaurant (before decision time is end)
+     *
+     * @param restaurantId - ID (int) of restaurant
+     * @param authUser     - auth user credentials
+     * @return {@code 201 "Created"} and {@link Vote} entity if vote was successful,
+     * {@code 200 OK} if vote change was successful or
+     * {@code 422 "Unprocessable Entity"} if vote change was not successful
+     */
+    @RequestMapping(value = "/restaurant/{restaurantId}", method = {RequestMethod.PUT, RequestMethod.POST})
+    public ResponseEntity<Vote> vote(@PathVariable Integer restaurantId, @AuthenticationPrincipal AuthorizedUser authUser) {
         int userId = authUser.getId();
-        log.info("vote for restaurant {} by user {}", restaurantId, userId);
+        log.info("vote for a restaurant {} by user {}", restaurantId, userId);
         Vote vote = service.vote(userId, restaurantId);
-
         if (vote != null){
             URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
                 .path(REST_URL + "/{id}")
                 .buildAndExpand(vote.getId()).toUri();
 
-            return ResponseEntity.created(uriOfNewResource).body(vote);
+            return vote.isUpdated() ?
+                ResponseEntity.ok().body(vote) :
+                ResponseEntity.created(uriOfNewResource).body(vote);
         }
-        return ResponseEntity.status(HttpStatus.LOCKED).body(null);
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(null);
     }
 
+    // TODO: 13.03.2020 add caching
+
+    /**
+     * Get votes (for today or in specified dates diapason) for a restaurant as array
+     *
+     * @param restaurantId - ID ({@literal int}) of restaurant
+     * @param dateFrom     -  start date for get votes (LocalDate), not required
+     * @param dateTo       -  end date for get votes (LocalDate), not required
+     * @return List of {@link Vote votes} for a restaurant with specified restaurantId
+     */
+    @GetMapping(value = "/restaurant/{restaurantId}")
+    public List<Vote> getForRestaurant(@PathVariable int restaurantId,
+                                       @RequestParam(required = false)
+                                       @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dateFrom,
+                                       @RequestParam(required = false)
+                                       @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dateTo) {
+        dateFrom = (dateFrom == null) ? LocalDate.now() : dateFrom;
+        dateTo = (dateTo == null) ? LocalDate.now() : dateTo;
+        log.info("get votes for a restaurant {} and date between {} and {}", restaurantId, dateFrom, dateTo);
+        return service.getForRestaurantAndDate(restaurantId, dateFrom, dateTo);
+    }
+
+    /**
+     * Get own votes (all or for specified date) as array
+     *
+     * @param date     - date for which to get votes
+     * @param authUser - auth user credentials
+     * @return List of own {@link Vote votes}
+     */
+    @GetMapping()
+    public List<Vote> getOwn(@RequestParam(required = false)
+                             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
+                             @AuthenticationPrincipal AuthorizedUser authUser) {
+        int userId = authUser.getId();
+        log.info("get votes for user {} and date {}", userId, date);
+        return (date == null)
+            ? service.getAllForUser(userId)
+            : Collections.singletonList(service.getForUserAndDate(userId, date));
+    }
+
+    /**
+     * Delete own vote for today (before decision time is end)
+     *
+     * @param authUser - auth user credentials
+     * @return {@code 204 "No Content"} if vote deleting was successful or
+     * {@code 422 "Unprocessable Entity"} if vote deleting was not successful
+     */
     @DeleteMapping()
-    public ResponseEntity delete(@AuthenticationPrincipal AuthorizedUser authUser) {
+    public ResponseEntity<Void> delete(@AuthenticationPrincipal AuthorizedUser authUser) {
         return service.delete(authUser.getId(), LocalDate.now())
             ? ResponseEntity.status(HttpStatus.NO_CONTENT).body(null)
-            : ResponseEntity.status(HttpStatus.LOCKED).body(null);
-    }
-
-    @GetMapping()
-    public Vote get(@RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
-                    @AuthenticationPrincipal AuthorizedUser authUser) {
-
-        int userId = authUser.getId();
-        date = (date == null) ? LocalDate.now() : date;
-        //        LocalDate currentDate = date.orElse(LocalDate.now());
-        log.info("get vote for user {} and date {}", userId, date);
-        return service.getForUserAndDate(userId, date);
+            : ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(null);
     }
 }

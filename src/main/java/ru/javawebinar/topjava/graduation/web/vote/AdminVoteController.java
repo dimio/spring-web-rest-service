f474e313a583fadf293c916cfa17e51cfc12a081
Dimio <dimio@dimio.org>
2020-03-15 21:37:34 +0300

    refactor(Controller): accord Admin controllers for new API
    AdminVoteController: added possibility to get all votes for restaurant and get user votes (all or for specified date)

--- src/main/java/ru/javawebinar/topjava/graduation/web/vote/AdminVoteController.java
+++ src/main/java/ru/javawebinar/topjava/graduation/web/vote/AdminVoteController.java
@@ -9,12 +9,13 @@ import ru.javawebinar.topjava.graduation.model.Vote;
 import ru.javawebinar.topjava.graduation.service.VoteService;
 
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = AdminVoteController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
 public class AdminVoteController {
-    static final String REST_URL = "/rest/admin";
+    static final String REST_URL = "/rest/admin/votes";
 
     private final Logger log = LoggerFactory.getLogger(getClass());
 
@@ -24,35 +25,32 @@ public class AdminVoteController {
         this.service = service;
     }
 
-    @GetMapping(value = "/user/{userId}/votes/all")
-    public List<Vote> getAllForUser(@PathVariable int userId) {
-        log.info("get all votes for user {}", userId);
-        return service.getAllForUser(userId);
+    /**
+     * Get user votes (all or for specified date) as array
+     *
+     * @param userId - user ID (int)
+     * @param date   - date for which to get votes ({@literal LocalDate}), not required
+     * @return List of user votes {@link Vote votes}
+     */
+    @GetMapping(value = "/user/{userId}")
+    public List<Vote> getForUserAndDate(@PathVariable int userId,
+                                        @RequestParam(required = false)
+                                        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+        log.info("get votes for user {} and date {}", userId, date);
+        return (date == null) ?
+            service.getAllForUser(userId) :
+            Collections.singletonList(service.getForUserAndDate(userId, date));
     }
 
-    @GetMapping(value = "/user/{userId}/votes")
-    public Vote getForUserAndDate(@PathVariable int userId,
-                                  @RequestParam(required = false)
-                                  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
-
-        date = (date == null) ? LocalDate.now() : date;
-        log.info("get all votes for user {} and date {}", userId, date);
-        return service.getForUserAndDate(userId, date);
-    }
-
-    @GetMapping(value = "/restaurant/{restaurantId}/votes/all")
+    /**
+     * Get all votes for a restaurant as array
+     *
+     * @param restaurantId - ID ({@literal int}) of restaurant
+     * @return List of votes {@link Vote votes}
+     */
+    @GetMapping(value = "/restaurant/{restaurantId}")
     public List<Vote> getAllForRestaurant(@PathVariable int restaurantId) {
-        log.info("get all votes for restaurant {}", restaurantId);
+        log.info("get all votes for a restaurant {}", restaurantId);
         return service.getAllForRestaurant(restaurantId);
-    }
-
-    @GetMapping(value = "/restaurant/{restaurantId}/votes")
-    public List<Vote> getForRestaurantAndDate(@PathVariable int restaurantId,
-                                              @RequestParam(required = false)
-                                              @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
-
-        date = (date == null) ? LocalDate.now() : date;
-        log.info("get all votes for restaurant {} and date {}", restaurantId, date);
-        return service.getForRestaurantAndDate(restaurantId, date);
     }
 }
